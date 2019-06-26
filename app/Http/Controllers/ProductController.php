3e403bf9c06c9ef42e2e75c7c34e5284bf8125d9
App Support <appsupport@daystarng.org>
2019-06-26 18:58:10 +0100

    Modified Product Details

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,7 +11,7 @@ class ProductController extends Controller
     
     public function index()
     {
-        return Product::all();
+        return ProductResource::collection(Product::all());
     }
 
     /**
--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -4,15 +4,19 @@ namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductResource extends JsonResource {
+class ProductResource extends JsonResource
+{
 
-    public function toArray($request) {
+    public function toArray($request)
+    {
         return [
+            'product_id' => $this->id,
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
             'discount' => $this->discount,
+            'total_price' => round(( 1 - ($this->discount / 100)) * $this->price,2),
             'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating yet',
             'href' => [
                 'reviews' => route('reviews.index', $this->id)
